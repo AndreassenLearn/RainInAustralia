bc42ee105331fe0b891cb91e9bcaaefdde732296
AndreassenLearn <simo531i@elevcampus.dk>
2023-04-24 16:06:02 +0200

    Cleaning Blazor

--- RainInAustraliaBlazor/Program.cs
+++ RainInAustraliaBlazor/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using RainInAustraliaBlazor.Data;
 using RainInAustraliaBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<IAussieWeatherService, AussieWeatherService>();
 
 var app = builder.Build();
